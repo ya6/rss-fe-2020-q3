7ae9976512ba774baeab3a75c3f6acbc6ee2a3c0
YA <vector_cp@mail.ru>
2020-11-26 00:16:44 +0300

    init: webpack set env: intermediate point #3 - add dev(no dev-server) & prod mode

--- english-for-kids/webpack.dev.js
+++ english-for-kids/webpack.dev.js
@@ -3,7 +3,6 @@ const common = require('./webpack.common');
 const {
   merge
 } = require('webpack-merge');
-const  HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
