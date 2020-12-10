c7a488c6ff1ec8f178612bee006449193d12483e
YA <vector_cp@mail.ru>
2020-12-10 09:38:17 +0300

    feat: add menu icons

--- english-for-kids/src/classes/menu.js
+++ english-for-kids/src/classes/menu.js
@@ -11,6 +11,7 @@ export default class Menu {
         };
 
         container.innerHTML = '';
+        let ind = 1;
         for (let menu_item of pageData['menu']) {
             let li = document.createElement('li')
             if (menu_item === pageData['page']) {
@@ -18,10 +19,15 @@ export default class Menu {
             }
             if (menu_item === 'delimiter') {
                 li.innerHTML = `<hr class="bg-light">`;
-            } else{ li.innerHTML = `<a class="menu__item ${active}" href="#">${menu_item}</a>`;}
+            } else {
+                li.innerHTML = `<div class="d-flex">
+                <div class="menu__icon mx-1" style="background-image: url(./assets/img/icon/icon_menu_${ind}.png)"></div>
+                <a class="menu__item ${active}" href="#">${menu_item}</a>
+                </div>`;
+            }
             active = '';
             container.appendChild(li);
-
+            ind += 1;
         }
 
     }
